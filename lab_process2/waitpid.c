ccd8b93a88780dc37220f65469f4344ef7311275
ilkerdemirkol <66560671+ilkerdemirkol@users.noreply.github.com>
2022-11-13 14:15:27 +0100

    Update waitpid.c

--- lab_process2/waitpid.c
+++ lab_process2/waitpid.c
@@ -6,25 +6,25 @@
 
 int main()
 {
-	int i, stat;
-	pid_t pid[5];
-	for (i=0; i<5; i++)
-	{
-		if ((pid[i] = fork()) == 0)
-		{
-			sleep(1);
-			exit(100 + i);
-		}
-	}
+    int i, stat;
+    pid_t pid[5];
+    for (i=0; i<5; i++)
+    {
+        if ((pid[i] = fork()) == 0)
+        {
+            sleep(1);
+            exit(100 + i);
+        }
+    }
 
-	// Using waitpid() and printing exit status
-	// of children.
-	for (i=0; i<5; i++)
-	{
-		pid_t cpid = waitpid(pid[i], &stat, 0);
-		if (WIFEXITED(stat))
-			printf("Child %d terminated with status: %d\n",
-				cpid, WEXITSTATUS(stat));
-	}
-	return 0;
+    // Using waitpid() and printing exit status
+    // of children.
+    for (i=0; i<5; i++)
+    {
+        pid_t cpid = waitpid(pid[i], &stat, 0);
+        if (WIFEXITED(stat))
+            printf("Child %d terminated with status: %d\n",
+                cpid, WEXITSTATUS(stat));
+    }
+    return 0;
 }
